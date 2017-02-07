5a2dbb0aa46694c19a91df224f6d2dc2f46abafe
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-02-06 23:11:55 -0600

    Looked at trying to get the controllers working for the pages that I had previously worked on. Made some progress but still am not able to get them working, read some good links on the different components that are being used in the workings of this process to try to understand exactly what is going on.

--- endpoints-skeleton-archetype/src/main/webapp/js/application.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/application.js
@@ -15,12 +15,35 @@ var app = angular.module('schedulerApplication',
 	                    controller: 'AddEmployeeCtrl'
 	                }).
 	                when('/', {
-	                    templateUrl: '/partials/login.html'
+	                    templateUrl: '/partials/homeIndex.html'
 	                }).
 	                otherwise({
 	                    redirectTo: '/'
 	                });
 	        }]);
+
+
+/**
+ * @ngdoc filter
+ * @name startFrom
+ *
+ * @description
+ * A filter that extracts an array from the specific index.
+ *
+ */
+app.filter('startFrom', function () {
+    /**
+     * Extracts an array from the specific index.
+     *
+     * @param {Array} data
+     * @param {Integer} start
+     * @returns {Array|*}
+     */
+    var filter = function (data, start) {
+        return data.slice(start);
+    }
+    return filter;
+});
 
 
 /**
@@ -35,5 +58,60 @@ app.constant('HTTP_ERRORS', {
     'UNAUTHORIZED': 401
 });
 
+
+/**
+ * @ngdoc service
+ * @name oauth2Provider
+ *
+ * @description
+ * Service that holds the OAuth2 information shared across all the pages.
+ *
+ */
+app.factory('oauth2Provider', function ($modal) {
+    var oauth2Provider = {
+        CLIENT_ID: '1032207459940-kl5tt995gsvp5j15ec0stmoastj68ame.apps.googleusercontent.com',
+        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
+        signedIn: false
+    };
+
+    /**
+     * Calls the OAuth2 authentication method.
+     */
+    oauth2Provider.signIn = function (callback) {
+        gapi.auth.signIn({
+            'clientid': oauth2Provider.CLIENT_ID,
+            'cookiepolicy': 'single_host_origin',
+            'accesstype': 'online',
+            'approveprompt': 'auto',
+            'scope': oauth2Provider.SCOPES,
+            'callback': callback
+        });
+    };
+
+    /**
+     * Logs out the user.
+     */
+    oauth2Provider.signOut = function () {
+        gapi.auth.signOut();
+        // Explicitly set the invalid access token in order to make the API calls fail.
+        gapi.auth.setToken({access_token: ''})
+        oauth2Provider.signedIn = false;
+    };
+
+    /**
+     * Shows the modal with Google+ sign in button.
+     *
+     * @returns {*|Window}
+     */
+    oauth2Provider.showLoginModal = function() {
+        var modalInstance = $modal.open({
+            templateUrl: '/partials/login.modal.html',
+            controller: 'OAuth2LoginModalCtrl'
+        });
+        return modalInstance;
+    };
+
+    return oauth2Provider;
+});
 
 
--- endpoints-skeleton-archetype/src/main/webapp/js/controllers.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/controllers.js
@@ -19,7 +19,35 @@ var schedulerApplication = schedulerApplication || {};
 schedulerApplication.controllers = angular.module('schedulerController', ['ui.bootstrap']);
 
 
-conferenceApp.controllers.controller('AddEmployeeCtrl',
-	    function ($scope, $modalInstance, $rootScope, oauth2Provider) {
+
+/**
+ * @ngdoc controller
+ * @name RootCtrl
+ *
+ * @description
+ * The root controller having a scope of the body element and methods used in the application wide
+ * such as user authentications.
+ *
+ */
+conferenceApp.controllers.controller('RootCtrl', function ($scope, $location, oauth2Provider) {
+
+    /**
+     * Returns if the viewLocation is the currently viewed page.
+     *
+     * @param viewLocation
+     * @returns {boolean} true if viewLocation is the currently viewed page. Returns false otherwise.
+     */
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+
+
+
+});
+
+
+
+conferenceApp.controllers.controller('AddEmployeeCtrl', function ($scope,  $location) {
 	 
+
 	    });
