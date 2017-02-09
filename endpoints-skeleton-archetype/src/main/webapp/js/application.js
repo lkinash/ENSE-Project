01a3b9dc186e0db7530812411edc1b2157dd76ef
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-02-09 16:42:00 -0600

    Added controllers for the rest of the pages, and routing parameters for all of the rest of the pages to the javascript files.

--- endpoints-skeleton-archetype/src/main/webapp/js/application.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/application.js
@@ -9,7 +9,7 @@ app.controller('MainController', function($scope, $route, $routeParams, $locatio
      $scope.$routeParams = $routeParams;
  });
  
-  app.controller('ViewEmployeeController', function($scope, $route, $routeParams, $location) {
+ app.controller('ViewEmployeeController', function($scope, $route, $routeParams, $location) {
 	 $scope.$route = $route;
 	 $scope.$location = $location;
 	 $scope.$routeParams = $routeParams;
@@ -50,39 +50,201 @@ app.controller('MainController', function($scope, $route, $routeParams, $locatio
      $scope.$location = $location;
      $scope.$routeParams = $routeParams;
  });
- 
+
+ app.controller('ForgotPasswordController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ClientEditProfileController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ClientLoginController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ClientBookingController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ClientCancelAppointmentController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ClientBookAppointmentController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('AdminEditProfileController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+  app.controller('ViewClientAppointmentController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+      
+  app.controller('HomeController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+   app.controller('LoginController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+   app.controller('PendingController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  }); 
+  
+  app.controller('RejectController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+  
+   app.controller('SignupController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });     
+  
+   app.controller('ViewAccountController', function($scope, $route, $routeParams, $location) {
+      $scope.$route = $route;
+      $scope.$location = $location;
+      $scope.$routeParams = $routeParams;
+  });
+
+
+
  
  app.config(function($routeProvider, $locationProvider) {
 	 $routeProvider
 
-     .when('/admin/viewEmployee', {
-         templateUrl: 'partials/viewEmployeeAdmin.html',
-         controller: 'ViewEmployeeController'
-     })
-     
-     .when('/admin/viewRoom', {
-         templateUrl: 'partials/viewRoomAdmin.html',
-         controller: 'ViewRoomController'
-     })
-     .when('/admin/viewService', {
-         templateUrl: 'partials/viewServiceAdmin.html',
-         controller: 'ViewServiceController'
-     })
-     .when('/admin/addEmployee', {
-         templateUrl: 'partials/addEmployeeAdmin.html',
-         controller: 'AddEmployeeController'
-     })
+	 
      .when('/admin/addAdmin', {
          templateUrl: 'partials/addAdminUser.html',
          controller: 'AddAdminController'
      })
+     
+     .when('/admin/addEmployee', {
+         templateUrl: 'partials/addEmployeeAdmin.html',
+         controller: 'AddEmployeeController'
+     })
+
      .when('/admin/addRoom', {
          templateUrl: 'partials/addRoomAdmin.html',
          controller: 'AddRoomController'
      })
+     
      .when('/admin/addService', {
          templateUrl: 'partials/addServiceAdmin.html',
          controller: 'AddServiceController'
+     })
+     
+     .when('/admin/editProfile', {
+         templateUrl: 'partials/adminEditProfile.html',
+         controller: 'AdminEditProfileController'
+     })
+     
+     .when('/client/bookAppointment', {
+         templateUrl: 'partials/bookAppointment.html',
+         controller: 'ClientBookAppointmentController'
+     })
+     
+     .when('/client/cancelAppointment', {
+         templateUrl: 'partials/cancelAppointment.html',
+         controller: 'ClientCancelAppointmentController'
+     })
+     
+     .when('/client/editBooking', {
+         templateUrl: 'partials/editBooking.html',
+         controller: 'ClientBookingController'
+     })
+     
+     .when('/client/login', {
+         templateUrl: 'partials/clientLogin.html',
+         controller: 'ClientLoginController'
+     })
+     
+     .when('/client/editProfile', {
+         templateUrl: 'partials/editProfile.html',
+         controller: 'ClientEditProfileController'
+     })
+     
+     .when('/forgotpassword', {
+         templateUrl: 'partials/forgotPassword.html',
+         controller: 'ForgotPasswordController'
+     })
+     
+     .when('/home', {
+         templateUrl: 'partials/homeIndex.html',
+         controller: 'HomeController'
+     })
+     
+     .when('/login', {
+         templateUrl: 'partials/Login.html',
+         controller: 'LoginController'
+     })
+     
+     .when('/admin/pending', {
+         templateUrl: 'partials/pendingAppAdmin.html',
+         controller: 'PendingController'
+     })    
+     
+     .when('/admin/reject', {
+         templateUrl: 'partials/rejectAppAdmin.html',
+         controller: 'RejectController'
+     })
+    
+     .when('/signup', {
+         templateUrl: 'partials/signUp.html',
+         controller: 'SignupController'
+     })
+    
+     .when('/admin/viewAccount', {
+         templateUrl: 'partials/viewAdminAccount.html',
+         controller: 'ViewAccountController'
+     })
+     
+     .when('/client/viewAppointment', {
+         templateUrl: 'partials/viewClientAppointment.html',
+         controller: 'ViewClientAppointmentController'
+     })
+     
+     .when('/admin/viewEmployee', {
+         templateUrl: 'partials/viewEmployeeAdmin.html',
+         controller: 'ViewEmployeeController'
+     })
+    
+     .when('/admin/viewRoom', {
+         templateUrl: 'partials/viewRoomAdmin.html',
+         controller: 'ViewRoomController'
+     })
+     
+     .when('/admin/viewService', {
+         templateUrl: 'partials/viewServiceAdmin.html',
+         controller: 'ViewServiceController'
      })
      
      .when('/', {
