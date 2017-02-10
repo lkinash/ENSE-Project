9cf18ed636db6725546af3d88ed26d1a87a3f2e9
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-02-09 21:38:18 -0600

    Added the code to a separate file to allow for easier collaboration. may have an issue, works in firefox but not in chrome at this point.

---
+++ endpoints-skeleton-archetype/src/main/webapp/js/ctrl.js
@@ -0,0 +1,141 @@
+
+'use strict';
+
+var schedulerApplication = schedulerApplication || {};
+
+schedulerApplication.controllers = angular.module('schedulerControllers', ['ui.bootstrap']);
+
+	          
+schedulerApplication.controllers.controller('MainController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('ViewEmployeeController', function($scope, $route, $routeParams, $location) {
+	 $scope.$route = $route;
+	 $scope.$location = $location;
+	 $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('AddEmployeeController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('ViewServiceController', function($scope, $route, $routeParams, $location) {
+	 $scope.$route = $route;
+	 $scope.$location = $location;
+	 $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('AddServiceController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('ViewRoomController', function($scope, $route, $routeParams, $location) {
+	 $scope.$route = $route;
+	 $scope.$location = $location;
+	 $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('AddRoomController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('AddAdminController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+});
+
+schedulerApplication.controllers.controller('ForgotPasswordController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ClientEditProfileController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ClientLoginController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ClientBookingController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ClientCancelAppointmentController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ClientBookAppointmentController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('AdminEditProfileController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+ schedulerApplication.controllers.controller('ViewClientAppointmentController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+     
+ schedulerApplication.controllers.controller('HomeController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+  schedulerApplication.controllers.controller('LoginController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+  schedulerApplication.controllers.controller('PendingController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ }); 
+ 
+ schedulerApplication.controllers.controller('RejectController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+  schedulerApplication.controllers.controller('SignupController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });     
+ 
+  schedulerApplication.controllers.controller('ViewAccountController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ });
+ 
+	        
