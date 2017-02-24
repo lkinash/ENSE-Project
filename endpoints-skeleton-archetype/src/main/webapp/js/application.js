23fe2bb1cf2df0c77c21cb011eb077fa5b271e66
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-02-23 23:32:40 -0600

    Trying to get the logout button to work

--- endpoints-skeleton-archetype/src/main/webapp/js/application.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/application.js
@@ -1,280 +1,10 @@
 
   'use strict';
 
-var app = angular.module('schedulerApplication', ['ngRoute']);
-
-app.controller('MainController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
-     
-     
-     function onSignIn(googleUser) {
-    	  var profile = googleUser.getBasicProfile();
-    	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    	  console.log('Name: ' + profile.getName());
-    	  console.log('Image URL: ' + profile.getImageUrl());
-    	  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    	}
-     
-     function init() {
-    	  window.init();
-    	
-          gapi.client.load('admin', 'v1', null, '//' + window.location.host + '/_ah/api');
-    	
-     }
-     
-     /**
-      * Returns the OAuth2 signedIn state.
-      *
-      * @returns {oauth2Provider.signedIn|*} true if siendIn, false otherwise.
-      */
-     $scope.getSignedInState = function () {
-         return oauth2Provider.signedIn;
-     };
-
-     /**
-      * Calls the OAuth2 authentication method.
-      */
-     $scope.signIn = function () {
-         oauth2Provider.signIn(function () {
-             gapi.client.oauth2.userinfo.get().execute(function (resp) {
-                 $scope.$apply(function () {
-                     if (resp.email) {
-                         oauth2Provider.signedIn = true;
-                         $scope.alertStatus = 'success';
-                         $scope.rootMessages = 'Logged in with ' + resp.email;
-                     }
-                 });
-             });
-         });
-     };
-     
-     
-     /**
-      * Render the signInButton and restore the credential if it's stored in the cookie.
-      * (Just calling this to restore the credential from the stored cookie. So hiding the signInButton immediately
-      *  after the rendering)
-      */
-     $scope.initSignInButton = function () {
-         gapi.signin.render('signInButton', {
-             'callback': function () {
-                 jQuery('#signInButton button').attr('disabled', 'true').css('cursor', 'default');
-                 if (gapi.auth.getToken() && gapi.auth.getToken().access_token) {
-                     $scope.$apply(function () {
-                         oauth2Provider.signedIn = true;
-                     });
-                 }
-             },
-             'clientid': oauth2Provider.CLIENT_ID,
-             'cookiepolicy': 'single_host_origin',
-             'scope': oauth2Provider.SCOPES
-         });
-              
-     };
- });
-
-/**
- * @ngdoc service
- * @name oauth2Provider
- *
- * @description
- * Service that holds the OAuth2 information shared across all the pages.
- *
- */
-app.factory('oauth2Provider', function ($modal) {
-    var oauth2Provider = {
-        CLIENT_ID: '573107621545-m9kr231t053m9sprflhffl349vcqrcb6.apps.googleusercontent.com',
-        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
-        signedIn: false
-    };
-
-    /**
-     * Calls the OAuth2 authentication method.
-     */
-    oauth2Provider.signIn = function (callback) {
-        gapi.auth.signIn({
-            'clientid': oauth2Provider.CLIENT_ID,
-            'cookiepolicy': 'single_host_origin',
-            'accesstype': 'online',
-            'approveprompt': 'auto',
-            'scope': oauth2Provider.SCOPES,
-            'callback': callback
-        });
-    };
-
-    /**
-     * Logs out the user.
-     */
-    oauth2Provider.signOut = function () {
-        gapi.auth.signOut();
-        // Explicitly set the invalid access token in order to make the API calls fail.
-        gapi.auth.setToken({access_token: ''})
-        oauth2Provider.signedIn = false;
-    };
-
-    /**
-     * Shows the modal with Google+ sign in button.
-     *
-     * @returns {*|Window}
-     */
-    oauth2Provider.showLoginModal = function() {
-        var modalInstance = $modal.open({
-            templateUrl: '/partials/login.modal.html',
-            controller: 'OAuth2LoginModalCtrl'
-        });
-        return modalInstance;
-    };
-
-    return oauth2Provider;
-});
- 
-app.controller('AddRoomController', function($scope, $route, $routeParams, $location) {
-
-	
-    $scope.addRoom = function() {
-	    roomForm = {
-	      "number" : $scope.number
-	    };
- 
-	 gapi.client.admin.addRoom(roomForm).execute();
-  };
- 
-});
-
- app.controller('ViewEmployeeController', function($scope, $route, $routeParams, $location) {
-
-	 
-	 
- });
-
- app.controller('AddEmployeeController', function($scope, $route, $routeParams, $location) {
-
-     
-     
-	  $scope.addEmployee = function() {
-	  	    $scope.employeeForm = {
-	  	      "email" : $scope.email,
-	  	      "name" : $scope.name,
-	  	      "password" : $scope.password
-	  	    };
-	   
-	  	 gapi.client.admin.addEmployee($scope.employeeForm).execute();
-	   };
- });
- 
- app.controller('ViewServiceController', function($scope, $route, $routeParams, $location) {
-
-
-	 
- });
-
- app.controller('AddServiceController', function($scope, $route, $routeParams, $location) {
-
-
-	 
- });
- 
- app.controller('ViewRoomController', function($scope, $route, $routeParams, $location) {
-
-
-	 
- });
-
- app.controller('AddAdminController', function($scope, $route, $routeParams, $location) {
-
-
-	 
- });
-
- app.controller('ForgotPasswordController', function($scope, $route, $routeParams, $location) {
-
-
-	 
-  });
-  
-  app.controller('ClientEditProfileController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-  app.controller('ClientLoginController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-  app.controller('ClientBookingController', function($scope, $route, $routeParams, $location) {
-  
-
-	  
-  });
-  
-  app.controller('ClientCancelAppointmentController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-  app.controller('ClientBookAppointmentController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-  app.controller('AdminEditProfileController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-  app.controller('ViewClientAppointmentController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-      
-  app.controller('HomeController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-   app.controller('LoginController', function($scope, $route, $routeParams, $location) {
-
-
-	   
-  });
-  
-   app.controller('PendingController', function($scope, $route, $routeParams, $location) {
-
-
-	   
-  }); 
-  
-  app.controller('RejectController', function($scope, $route, $routeParams, $location) {
-
-
-	  
-  });
-  
-   app.controller('SignupController', function($scope, $route, $routeParams, $location) {
-
-
-	   
-  });     
-  
-   app.controller('ViewAccountController', function($scope, $route, $routeParams, $location) {
-
-
-  });
-
-
- 
- app.config(function($routeProvider, $locationProvider) {
-	 $routeProvider
-
+var app = angular.module('conferenceApp', ['schedulerControllers', 'ngRoute', 'ui.bootstrap']).
+config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
 	 
      .when('/admin/addAdmin', {
          templateUrl: 'partials/addAdminUser.html',
@@ -382,8 +112,7 @@ app.controller('AddRoomController', function($scope, $route, $routeParams, $loca
      })
      
      .when('/', {
-         templateUrl: 'partials/viewCalendarMainAdmin.html',
-         controller: 'MainController'
+         templateUrl: 'partials/viewCalendarMainAdmin.html'
      })
      .otherwise({  redirectTo: '/' });
 	 
@@ -476,4 +205,59 @@ app.controller('OAuth2LoginModalCtrl',
 		        };
 		    });
  
+
+/**
+ * @ngdoc service
+ * @name oauth2Provider
+ *
+ * @description
+ * Service that holds the OAuth2 information shared across all the pages.
+ *
+ */
+app.factory('oauth2Provider', function ($modal) {
+    var oauth2Provider = {
+        CLIENT_ID: '573107621545-m9kr231t053m9sprflhffl349vcqrcb6.apps.googleusercontent.com',
+        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
+        signedIn: false
+    };
+
+    /**
+     * Calls the OAuth2 authentication method.
+     */
+    oauth2Provider.signIn = function (callback) {
+        gapi.auth.signIn({
+            'clientid': oauth2Provider.CLIENT_ID,
+            'cookiepolicy': 'single_host_origin',
+            'accesstype': 'online',
+            'approveprompt': 'auto',
+            'scope': oauth2Provider.SCOPES,
+            'callback': callback
+        });
+    };
+
+    /**
+     * Logs out the user.
+     */
+    oauth2Provider.signOut = function () {
+        gapi.auth.signOut();
+        // Explicitly set the invalid access token in order to make the API calls fail.
+        gapi.auth.setToken({access_token: ''})
+        oauth2Provider.signedIn = false;
+    };
+
+    /**
+     * Shows the modal with Google+ sign in button.
+     *
+     * @returns {*|Window}
+     */
+    oauth2Provider.showLoginModal = function() {
+        var modalInstance = $modal.open({
+            templateUrl: '/partials/login.modal.html',
+            controller: 'OAuth2LoginModalCtrl'
+        });
+        return modalInstance;
+    };
+
+    return oauth2Provider;
+});
 
--- endpoints-skeleton-archetype/src/main/webapp/js/ctrl.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/ctrl.js
@@ -1,204 +1,269 @@
-//'use strict';
-
-//var schedulerApplication = schedulerApplication || {};
-
-//schedulerApplication.controllers = angular.module('schedulerControllers', ['ui.bootstrap']);
-/*
 'use strict';
 
 var schedulerApplication = schedulerApplication || {};
 
-schedulerApplication.controllers = angular.module('schedulerControllers', ['ui.bootstrap']);
+schedulerApplication.controllers = angular.module('schedulerControllers', [ 'ui.bootstrap' ]);
 
-	          
-schedulerApplication.controllers.controller('MainController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('MainController', function($scope,
+		$route, $routeParams, $location, $log, oauth2Provider, HTTP_ERRORS) {
+
+	/**
+	 * Returns if the viewLocation is the currently viewed page.
+	 * 
+	 * @param viewLocation
+	 * @returns {boolean} true if viewLocation is the currently viewed page.
+	 *          Returns false otherwise.
+	 */
+	$scope.isActive = function(viewLocation) {
+		return viewLocation === $location.path();
+	};
+
+	/**
+	 * Returns the OAuth2 signedIn state.
+	 * 
+	 * @returns {oauth2Provider.signedIn|*} true if siendIn, false otherwise.
+	 */
+	$scope.getSignedInState = function() {
+		return oauth2Provider.signedIn;
+	};
+
+	/**
+	 * Calls the OAuth2 authentication method.
+	 */
+	$scope.signIn = function() {
+		oauth2Provider.signIn(function() {
+			gapi.client.oauth2.userinfo.get().execute(function(resp) {
+				$scope.$apply(function() {
+					if (resp.email) {
+						oauth2Provider.signedIn = true;
+						$scope.alertStatus = 'success';
+						$scope.rootMessages = 'Logged in with ' + resp.email;
+					}
+				});
+			});
+		});
+	};
+
+	/**
+	 * Render the signInButton and restore the credential if it's stored in the
+	 * cookie. (Just calling this to restore the credential from the stored
+	 * cookie. So hiding the signInButton immediately after the rendering)
+	 */
+	$scope.initSignInButton = function() {
+		gapi.signin.render('signInButton', {
+			'callback' : function() {
+				jQuery('#signInButton button').attr('disabled', 'true').css(
+						'cursor', 'default');
+				if (gapi.auth.getToken() && gapi.auth.getToken().access_token) {
+					$scope.$apply(function() {
+						oauth2Provider.signedIn = true;
+					});
+				}
+			},
+			'clientid' : oauth2Provider.CLIENT_ID,
+			'cookiepolicy' : 'single_host_origin',
+			'scope' : oauth2Provider.SCOPES
+		});
+	};
+
+	/**
+	 * Logs out the user.
+	 */
+	$scope.signOut = function() {
+		oauth2Provider.signOut();
+		$scope.alertStatus = 'success';
+		$scope.rootMessages = 'Logged out';
+	};
+
+	function onSignIn(googleUser) {
+		var profile = googleUser.getBasicProfile();
+		console.log('ID: ' + profile.getId()); // Do not send to your backend!
+												// Use an ID token instead.
+		console.log('Name: ' + profile.getName());
+		console.log('Image URL: ' + profile.getImageUrl());
+		console.log('Email: ' + profile.getEmail()); // This is null if the
+														// 'email' scope is not
+														// present.
+	}
+
+	function init() {
+		window.init();
+
+		gapi.client.load('admin', 'v1', null, '//' + window.location.host
+				+ '/_ah/api');
+
+	}
+
+	/**
+	 * Returns the OAuth2 signedIn state.
+	 * 
+	 * @returns {oauth2Provider.signedIn|*} true if siendIn, false otherwise.
+	 */
+	$scope.getSignedInState = function() {
+		return oauth2Provider.signedIn;
+	};
+
+	/**
+	 * Calls the OAuth2 authentication method.
+	 */
+	$scope.signIn = function() {
+		oauth2Provider.signIn(function() {
+			gapi.client.oauth2.userinfo.get().execute(function(resp) {
+				$scope.$apply(function() {
+					if (resp.email) {
+						oauth2Provider.signedIn = true;
+						$scope.alertStatus = 'success';
+						$scope.rootMessages = 'Logged in with ' + resp.email;
+					}
+				});
+			});
+		});
+	};
+
+	/**
+	 * Render the signInButton and restore the credential if it's stored in the
+	 * cookie. (Just calling this to restore the credential from the stored
+	 * cookie. So hiding the signInButton immediately after the rendering)
+	 */
+	$scope.initSignInButton = function() {
+		gapi.signin.render('signInButton', {
+			'callback' : function() {
+				jQuery('#signInButton button').attr('disabled', 'true').css(
+						'cursor', 'default');
+				if (gapi.auth.getToken() && gapi.auth.getToken().access_token) {
+					$scope.$apply(function() {
+						oauth2Provider.signedIn = true;
+					});
+				}
+			},
+			'clientid' : oauth2Provider.CLIENT_ID,
+			'cookiepolicy' : 'single_host_origin',
+			'scope' : oauth2Provider.SCOPES
+		});
+
+	};
+
 });
 
-schedulerApplication.controllers.controller('ViewEmployeeController', function($scope, $route, $routeParams, $location) {
-	 $scope.$route = $route;
-	 $scope.$location = $location;
-	 $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('AddRoomController', function(
+		$scope, $route, $routeParams, $location) {
+
+	$scope.addRoom = function() {
+		roomForm = {
+			"number" : $scope.number
+		};
+
+		gapi.client.admin.addRoom(roomForm).execute();
+	};
+
 });
 
-schedulerApplication.controllers.controller('AddEmployeeController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('ViewEmployeeController', function(
+		$scope, $route, $routeParams, $location) {
+
 });
 
-schedulerApplication.controllers.controller('ViewServiceController', function($scope, $route, $routeParams, $location) {
-	 $scope.$route = $route;
-	 $scope.$location = $location;
-	 $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('AddEmployeeController', function(
+		$scope, $route, $routeParams, $location) {
+
+	$scope.addEmployee = function() {
+		$scope.employeeForm = {
+			"email" : $scope.email,
+			"name" : $scope.name,
+			"password" : $scope.password
+		};
+
+		gapi.client.admin.addEmployee($scope.employeeForm).execute();
+	};
 });
 
-schedulerApplication.controllers.controller('AddServiceController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('ViewServiceController', function(
+		$scope, $route, $routeParams, $location) {
+
 });
 
-schedulerApplication.controllers.controller('ViewRoomController', function($scope, $route, $routeParams, $location) {
-	 $scope.$route = $route;
-	 $scope.$location = $location;
-	 $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('AddServiceController', function(
+		$scope, $route, $routeParams, $location) {
+
 });
 
-schedulerApplication.controllers.controller('AddRoomController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('ViewRoomController', function(
+		$scope, $route, $routeParams, $location) {
+
 });
 
-schedulerApplication.controllers.controller('AddAdminController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+schedulerApplication.controllers.controller('AddAdminController', function(
+		$scope, $route, $routeParams, $location) {
+
 });
 
-schedulerApplication.controllers.controller('ForgotPasswordController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ClientEditProfileController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ClientLoginController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ClientBookingController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ClientCancelAppointmentController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ClientBookAppointmentController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('AdminEditProfileController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- schedulerApplication.controllers.controller('ViewClientAppointmentController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
-     
- schedulerApplication.controllers.controller('HomeController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
-  schedulerApplication.controllers.controller('LoginController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
-  schedulerApplication.controllers.controller('PendingController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- }); 
- 
- schedulerApplication.controllers.controller('RejectController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
-  schedulerApplication.controllers.controller('SignupController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });     
- 
-  schedulerApplication.controllers.controller('ViewAccountController', function($scope, $route, $routeParams, $location) {
-     $scope.$route = $route;
-     $scope.$location = $location;
-     $scope.$routeParams = $routeParams;
- });
- 
- 
- */
-/**
- * @ngdoc controller
- * @name addRoomCtrl
- *
- * @description
- * A controller used for the AddRoomAdmin page.
- *//*
-schedulerApplication.controllers.controller('addRoomCtrl',
-    function ($scope, $log, oauth2Provider, HTTP_ERRORS) {
+schedulerApplication.controllers.controller('ForgotPasswordController',
+		function($scope, $route, $routeParams, $location) {
 
-        /**
-         * The Room object being edited in the page.
-         * @type {{}|*}
-         *//*
-        $scope.room = $scope.room || {};
-*/
+		});
 
-        /**
-         * Invokes the admin.addRoom API.
-         *
-         * @param conferenceForm the form object.
-         *//*
-        $scope.addRoom = function (RoomForm) {
-          /*  if (!$scope.isValidConference(RoomForm)) {
-                return;
-            }
-*//*
-            $scope.loading = true;
-            gapi.client.admin.addRoom($scope.room).
-                execute(function (resp) {
-                    $scope.$apply(function () {
-                        $scope.loading = false;
-                        if (resp.error) {
-                            // The request has failed.
-                            var errorMessage = resp.error.message || '';
-                            $scope.messages = 'Failed to add room : ' + errorMessage;
-                            $scope.alertStatus = 'warning';
-                            $log.error($scope.messages + ' room : ' + JSON.stringify($scope.Room));
+schedulerApplication.controllers.controller('ClientEditProfileController',
+		function($scope, $route, $routeParams, $location) {
 
-                            if (resp.code && resp.code == HTTP_ERRORS.UNAUTHORIZED) {
-                                oauth2Provider.showLoginModal();
-                                return;
-                            }
-                        } else {
-                            // The request has succeeded.
-                            $scope.messages = 'The Room has been created : ' + resp.result.name;
-                            $scope.alertStatus = 'success';
-                            $scope.submitted = false;
-                            $scope.Room = {};
-                            $log.info($scope.messages + ' : ' + JSON.stringify(resp.result));
-                        }
-                    });
-                });
-        };
-    });
-*/
-	        
+		});
+
+schedulerApplication.controllers.controller('ClientLoginController', function(
+		$scope, $route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('ClientBookingController',
+		function($scope, $route, $routeParams, $location) {
+
+		});
+
+schedulerApplication.controllers.controller(
+		'ClientCancelAppointmentController', function($scope, $route,
+				$routeParams, $location) {
+
+		});
+
+schedulerApplication.controllers.controller('ClientBookAppointmentController',
+		function($scope, $route, $routeParams, $location) {
+
+		});
+
+schedulerApplication.controllers.controller('AdminEditProfileController',
+		function($scope, $route, $routeParams, $location) {
+
+		});
+
+schedulerApplication.controllers.controller('ViewClientAppointmentController',
+		function($scope, $route, $routeParams, $location) {
+
+		});
+
+schedulerApplication.controllers.controller('HomeController', function($scope,
+		$route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('LoginController', function($scope,
+		$route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('PendingController', function(
+		$scope, $route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('RejectController', function(
+		$scope, $route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('SignupController', function(
+		$scope, $route, $routeParams, $location) {
+
+});
+
+schedulerApplication.controllers.controller('ViewAccountController', function(
+		$scope, $route, $routeParams, $location) {
+
+});
