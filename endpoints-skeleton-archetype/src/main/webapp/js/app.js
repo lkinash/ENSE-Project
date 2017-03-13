f561898fdab31c1454a53c9772a2ec6ad5228132
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-03-12 19:41:39 -0600

    Fixing login issue

--- endpoints-skeleton-archetype/src/main/webapp/js/app.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/app.js
@@ -199,7 +199,6 @@ app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
         CLIENT_ID: '573107621545-m9kr231t053m9sprflhffl349vcqrcb6.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
-        	//https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly',
         signedIn: false
     };
 
