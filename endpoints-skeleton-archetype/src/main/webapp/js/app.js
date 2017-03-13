dc904c871f5a4143d1b44e909bda1ff910ae30c1
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-03-12 18:05:47 -0600

    fix for sign in error

--- endpoints-skeleton-archetype/src/main/webapp/js/app.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/app.js
@@ -198,7 +198,8 @@ app.constant('HTTP_ERRORS', {
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
         CLIENT_ID: '573107621545-m9kr231t053m9sprflhffl349vcqrcb6.apps.googleusercontent.com',
-        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly',
+        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
+        	//https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly',
         signedIn: false
     };
 
