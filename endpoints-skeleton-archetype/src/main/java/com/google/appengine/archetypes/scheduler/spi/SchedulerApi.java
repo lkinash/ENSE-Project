3a9834cbea2eb5680d53945b81915620ab804637
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-03-14 23:49:00 -0600

    Fixing long type error

--- endpoints-skeleton-archetype/src/main/java/com/google/appengine/archetypes/scheduler/spi/SchedulerApi.java
+++ endpoints-skeleton-archetype/src/main/java/com/google/appengine/archetypes/scheduler/spi/SchedulerApi.java
@@ -1300,17 +1300,17 @@ public class SchedulerApi {
   	 */
   	
   	@ApiMethod(name = "admin.getServiceOfType", path = "admin.getServiceOfType", httpMethod = "get")
- 	public List<Service> getServicesOfType(final User user,	@Named("typeId") final long typeId) throws UnauthorizedException {
+ 	public List<Service> getServicesOfType(final User user,	@Named("typeId") final String typeId) throws UnauthorizedException {
 
         if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
         
-        
+
         Query<Service> query =  ofy().load().type(Service.class);
     	query = query.order("name");
 
-     	query = query.filter("typeId =", typeId);
+     	query = query.filter("typeId =", Long.valueOf(typeId));
     	
         return query.list();
         
