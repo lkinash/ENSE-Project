3742f25ef95aa106b16d28514e8505a4f38cb452
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-03-16 13:46:25 -0600

    Employee Test Fix

--- endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/EmployeeTest.java
+++ endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/EmployeeTest.java
@@ -1,7 +1,13 @@
 package com.google.appengine.archetypes.scheduler.entities;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 import java.util.List;
 
@@ -15,12 +21,27 @@ public class EmployeeTest {
 	private static final String CALENDARID = "12345678999";
 	private static final String NAME = "Test User";
 	private static final String USERID = "123458956";
-	private static final String EMPLOYEEID = "6665559";
-	
-	//private static final AdminClearances clearance
-	
+	private static final long EMPLOYEEID = 6665559;
 	
 	private Employee employee;
+
+	
+	 private final LocalServiceTestHelper testHelper =  new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+	    
+	 //Before the test is run set up the test data store helper and create a new instance of the SaleItem Object
+	 @Before
+	 public void setUp() throws Exception {
+		 testHelper.setUp();
+	     employee = new Employee(CALENDARID, NAME, USERID, null, EMPLOYEEID);
+	 }
+
+	 //After the test is run, user the helper to remove the data store entities that were involved in the test as they are unneeded 
+	 @After
+	 public void tearDown() throws Exception {
+		 testHelper.tearDown();
+	 }	
+	
 	
 	@Test
 	public void testGetters() throws Exception{
