a2844921bdd74d1790dd05a17c1507de834817fd
archanagumber <archana.gumber20@gmail.com>
2017-03-16 12:33:33 -0600

    Fixed the Room Test Case to remove error

--- endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/RoomTest.java
+++ endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/RoomTest.java
@@ -1,7 +1,13 @@
 package com.google.appengine.archetypes.scheduler.entities;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 import java.util.List;
 
@@ -17,6 +23,22 @@ public class RoomTest {
 	private static final String CALENDARID = "123458956";
 
 	private Room room;
+	private List<Long> serviceIds;
+	
+	private final LocalServiceTestHelper testHelper =  new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+	//Before the test is run set up the test data store helper and create a new instance of the SaleItem Object
+    @Before
+    public void setUp() throws Exception {
+        testHelper.setUp();
+        room = new Room(NUMBER,serviceIds,CALENDARID, ROOMID);
+    }
+
+    //After the test is run, user the helper to remove the data store entities that were involved in the test as they are unneeded 
+    @After
+    public void tearDown() throws Exception {
+        testHelper.tearDown();
+    }
 
 	@Test
 	public void testGetters() throws Exception {
