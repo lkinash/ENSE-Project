a3ebade3e5d5f5280be3e2b1f6b9efab860a2fa7
archanagumber <archana.gumber20@gmail.com>
2017-03-16 12:38:13 -0600

    Fixed Type Test Case to remove error

--- endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/TypeTest.java
+++ endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/TypeTest.java
@@ -1,7 +1,13 @@
 package com.google.appengine.archetypes.scheduler.entities;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 import java.util.List;
 
@@ -17,6 +23,21 @@ public class TypeTest {
 	private static final boolean ISSERVICE = true;
 	
 	private Type type;
+	
+	private final LocalServiceTestHelper testHelper =  new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+	//Before the test is run set up the test data store helper and create a new instance of the SaleItem Object
+    @Before
+    public void setUp() throws Exception {
+        testHelper.setUp();
+        type = new Type(ISSERVICE,TYPE,TYPEID);
+    }
+
+    //After the test is run, user the helper to remove the data store entities that were involved in the test as they are unneeded 
+    @After
+    public void tearDown() throws Exception {
+        testHelper.tearDown();
+    }
 	
 	@Test
 	public void testGetters() throws Exception{
