d967eec81181ad03ed603f1b1af5f58b097f3346
archanagumber <archana.gumber20@gmail.com>
2017-03-16 13:12:36 -0600

    Fixed Product test case to remove error

--- endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/ProductTest.java
+++ endpoints-skeleton-archetype/src/test/java/com/google/appengine/archetypes/scheduler/entities/ProductTest.java
@@ -1,9 +1,13 @@
 package com.google.appengine.archetypes.scheduler.entities;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 /*
  * @author Archana
@@ -17,14 +21,28 @@ public class ProductTest {
     private long PRODUCTID = 556345;
     private long TYPEID = 22656;
     
-    
     private Product product;
     
+    private final LocalServiceTestHelper testHelper =  new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+	//Before the test is run set up the test data store helper and create a new instance of the SaleItem Object
+    @Before
+    public void setUp() throws Exception {
+        testHelper.setUp();
+        product = new Product(BARCODENUM,PRODUCTID,NAME,TYPEID,PRICE);
+    }
+
+    //After the test is run, user the helper to remove the data store entities that were involved in the test as they are unneeded 
+    @After
+    public void tearDown() throws Exception {
+        testHelper.tearDown();
+    }
+  
     @Test
     public void testGetters() throws Exception{
     	assertEquals(BARCODENUM,product.getBarcodeNumber());
     	assertEquals(NAME,product.getName());
-    	assertEquals(PRICE,product.getPrice());
+    	assertEquals(PRICE,product.getPrice(), 0.0001);
     	assertEquals(PRODUCTID,product.getProductId());
     	assertEquals(TYPEID,product.getTypeId());
     	
