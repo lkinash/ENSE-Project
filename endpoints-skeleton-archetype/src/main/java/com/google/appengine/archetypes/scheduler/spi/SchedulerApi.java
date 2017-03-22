2b0ba4f953f0210100fe47965842e8b858a8d148
dbell0779 <dominique.belhumeur@gmail.com>
2017-03-22 13:50:28 -0600

    Revert "tried to edit remove service id as it was not workin"
    This reverts commit 93d03922e138c65fc41938332dd7789497c6bef8.

--- endpoints-skeleton-archetype/src/main/java/com/google/appengine/archetypes/scheduler/spi/SchedulerApi.java
+++ endpoints-skeleton-archetype/src/main/java/com/google/appengine/archetypes/scheduler/spi/SchedulerApi.java
@@ -898,13 +898,13 @@ public class SchedulerApi {
 	    }
 
 	    
-	    //long serviceId = removeServiceForm.getServiceId();
+	    long serviceId = removeServiceForm.getServiceId();
 	    
-	    Key<Service> key = Key.create(Service.class, removeServiceForm.getServiceId());
+	    Key<Service> key = Key.create(Service.class, serviceId);
 		
 	    ofy().delete().key(key).now();
 	    
-  		String change = "Remove Service. Service Id: " + removeServiceForm.getServiceId();
+  		String change = "Remove Service. Service Id: " + serviceId;
   		addChange(user, user.getUserId(), change);
   	    
 	    
