0fa309d875a610cc9d03ea759f5900690ff94937
Lindsey Kinash <kinash.lindsey@gmail.com>
2017-03-26 15:55:01 -0600

    Week day list

---
+++ endpoints-skeleton-archetype/src/main/java/com/google/appengine/archetypes/scheduler/list/WeekDay.java
@@ -0,0 +1,40 @@
+package com.google.appengine.archetypes.scheduler.list;
+
+public enum WeekDay {
+
+	/**
+	 * Description of SUNDAY.
+	 */
+	SUNDAY,					
+	
+	/**
+	 * Description of MONDAY.
+	 */
+	MONDAY,					
+	
+	/**
+	 * Description of TUESDAY.
+	 */
+	TUESDAY,					
+	
+	/**
+	 * Description of WEDNESDAY.
+	 */
+	WEDNESDAY,					
+	
+	/**
+	 * Description of THURSDAY.
+	 */
+	THURSDAY,					
+	
+	/**
+	 * Description of FRIDAY.
+	 */
+	FRIDAY,					
+	
+	/**
+	 * Description of SATURDAY.
+	 */
+	SATURDAY,
+	
+}
