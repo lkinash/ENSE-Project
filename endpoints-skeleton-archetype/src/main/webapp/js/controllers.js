618fb55aa90e442254d5a25ae45295c304499b75
dbell0779 <dominique.belhumeur@gmail.com>
2017-04-04 16:11:28 -0600

    Got editing a Client successfully working
    Got editing a Client successfully working

--- endpoints-skeleton-archetype/src/main/webapp/js/controllers.js
+++ endpoints-skeleton-archetype/src/main/webapp/js/controllers.js
@@ -1112,7 +1112,40 @@ conferenceApp.controllers.controller('AddRoomController', function ($scope, $log
 	  var clientForm={
 			  "clientId":tempId2
 	  };
-	
+	  var endYear = 2017;
+	   var startYear = 1920;
+	   $scope.selects = {
+
+			   days: function(){
+
+			     // Get number of days based on month + year 
+			     // (January = 31, February = 28, April = 30, February 2000 = 29) or 31 if no month selected yet
+			     var nbDays = new Date($scope.dateYear, $scope.dateMonth, 0).getDate() || 31;
+
+			     var daysList = [];
+			     for( var i = 1; i <= nbDays ; i++){
+			       var iS = i.toString();
+			       daysList.push( (iS.length < 2) ? '0' + iS : iS ); // Adds a leading 0 if single digit
+			     }
+			     return daysList;
+			   },
+			   months: function(){
+			     var monthList = [];
+			     for( var i = 1; i <= 12 ; i++){
+			       var iS = i.toString();
+			       monthList.push( (iS.length < 2) ? '0' + iS : iS ); // Adds a leading 0 if single digit
+			     }
+			     return monthList;
+			   },
+			   years: function(){
+			     var yearsList = [];
+			     for( var i = endYear; i >= startYear ; i--){
+			       yearsList.push( i.toString() );
+			     }
+			     return yearsList;
+			   }
+			 };
+	   
 		gapi.client.scheduler.client.getClientObject(clientForm).execute(function(resp){
 	 		$scope.clientOb=resp.result;
 			 $scope.$apply();
@@ -1121,10 +1154,13 @@ conferenceApp.controllers.controller('AddRoomController', function ($scope, $log
 			 $scope.lname=$scope.clientOb.lastName;
 			 $scope.phoneNumber=$scope.clientOb.phoneNumber;
 			 $scope.email=$scope.clientOb.email;
+			 $scope.dateYear=$scope.clientOb.birthdayBlock.year;
 			 $scope.dateMonth=$scope.clientOb.birthdayBlock.month;
+			 $scope.dateDay=$scope.clientOb.birthdayBlock.day;
 			 }
 	 	 });
-	$scope.nametest="sdkfjhdkdf";
+
+		
 	
 	 $scope.addClient=function(){
 			 var clientForm={};
